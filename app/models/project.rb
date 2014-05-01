a579e5029d4d8d24afc777791d5c043a792c84e2
Kruttik Aggarwal <kruttik.aggarwal@Macintosh.local>
2014-05-01 16:59:33 -0400

    Added UI componants

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,6 @@
 class Project
   include Mongoid::Document
+  include Mongoid::Timestamps
   field :name                 , type: String
   field :region_primary       , type: String
   field :vendor               , type: String
